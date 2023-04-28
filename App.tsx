1ba6219321d1fb491a2441a2c7b4f09b741ea1ee
fbourdenet <florian.bourdenet@gmail.com>
2023-04-28 11:58:49 +0200

    setup mmkv

--- App.tsx
+++ App.tsx
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { MMKV } from 'react-native-mmkv'
+
+export const storage = new MMKV()
+
+storage.set('user.name', 'Marc')
+
 export default function App() {
+  const [username, setUsername] = useState<string | undefined>();
+
+  useEffect(() => {
+    storage.set('user.name', 'Marc')
+    setUsername(storage.getString('user.name'))// 'Marc'
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {username &&
+        <Text>Welcome back {username} !</Text>
+      }
       <StatusBar style="auto" />
     </View>
   );
